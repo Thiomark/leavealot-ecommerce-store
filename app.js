6af8440638578500fe63cbc6de4634c497fee297
Itumeleng Tsoela <thiotsoela@gmail.com>
2021-03-25 08:10:49 +0200

    changes

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.use(express.json());
 
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/users', userRouter)
-app.all('*', (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: `Can't find ${req.originalUrl} on the server`
-    })
-})
+// app.all('*', (req, res, next) => {
+//     res.status(404).json({
+//         success: false,
+//         message: `Can't find ${req.originalUrl} on the server`
+//     })
+// })
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/client/build')))
